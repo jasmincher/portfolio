ff70f022b57c8ff6bce541b68f73c35ceb8823aa
Jas <c.jasminher@gmail.com>
2019-11-07 11:31:57 -0500

    Added home page and not found page

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+     this is the home page.
+    
+    </div>
+
+  );
+}
+
+export default Home;
---
+++ src/pages/NotFound.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function NotFound() {
+    return (
+        <div style={{textAlign: 'center', margin: '50px'}}>
+            <h1>Sorry Page Not Found </h1>
+            <br />
+            <h1>🤷🏻</h1>
+        </div>
+
+    );
+}
+
+
+export default NotFound;
