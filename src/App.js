e47cc960ce1d23303299dead30d1b4689b2dc414
Jas <c.jasminher@gmail.com>
2019-11-12 14:37:20 -0500

    Installed react router dom, and added routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-    Hello
+
+<Switch>
+  <Route exact path='/' component={Home} />
+  {/* <Route path='/' component={} />
+  <Route path='/' component={} /> */}
+  <Route component={NotFound} />
+
+</Switch>
+
+
     </div>
   );
 }
