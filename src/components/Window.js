baf5cf676cacb88558374215e1eaf8cbc097759d
Jas <c.jasminher@gmail.com>
2019-11-22 22:28:08 -0500

    Created a window component that will serve as a wrapper for other components, also added rnd to make the window draggable

---
+++ src/components/Window.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../sass/Window.scss';
+import { Rnd } from 'react-rnd';
+
+class Window extends React.Component {
+
+    render() {
+        return (
+
+            <Rnd>
+
+                <div className="window-container">
+                    {this.props.children} 
+                </div>
+
+            </Rnd>
+        )
+    }
+}
+
+
+
+
+
+export default Window;
