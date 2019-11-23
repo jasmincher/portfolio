2634a618ad68d86ec8e579768a546085f0b0df1d
Jas <c.jasminher@gmail.com>
2019-11-22 22:42:03 -0500

    Created functions to hide and show window when clicked on icon

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../sass/Home.scss';
+import Window from '../components/Window';
+import Notes from '../components/Notes';
+import Section from '../components/Section';
+import { Icons } from '../assets/data/Data'
+
 
 function Home() {
+
+  const [showWindow, setShowWindow] = useState(0);
+  const [currentWindow, setCurrentWindow] = useState(null);
+
+
+  const shouldShowWindow = (window) => {
+    setShowWindow(!showWindow);
+    if (window != null) {
+      setCurrentWindow(!null)
+    }
+
+    console.log("did should show window")
+  }
+
+
+  const hideWindow = () => {
+    setShowWindow(false);
+    setCurrentWindow(null);
+    console.log("did hide window")
+
+  }
+
+
+  const renderWindow = () => {
+    if (showWindow == true && currentWindow != null) {
+      return (
+        <Window>
+
+          <button onClick={hideWindow}>Hide</button>
+          <Notes />
+        </Window>
+
+
+
+      )
+
+    }
+    console.log("did render window")
+  }
+
+
+
+
   return (
-    <div>
-     this is the home page.
-    notebook theme...
+    <div className="home-container">
+
+
+      {renderWindow()}
+
+
+      <div onClick={() => { (showWindow == true) ? hideWindow() : shouldShowWindow() }}>
+
+
+        <Section icons={Icons} onItemClick={shouldShowWindow} />
+
+      </div>
+
+
+
+
     </div>
 
   );
