60bcfbb56c17d4b3405b14a5189ab8825a1430dc
Jas <c.jasminher@gmail.com>
2019-11-22 22:22:15 -0500

    Added images for icons, and stored them in an object, in Data.js

---
+++ src/assets/data/Data.js
@@ -0,0 +1,45 @@
+import notes from '../images/Text 2.png';
+import folder from '../images/Folder.png';
+import email from '../images/Message.png';
+import pics from '../images/Pics.png';
+import { isUserWhitespacable } from '@babel/types';
+
+
+export const Icons = 
+[
+    {
+        title: 'Notes',
+        icon: notes,
+        window: '..'
+
+    },
+    
+    {
+        title: 'Mail',
+        icon: email,
+        window: '..',
+
+    },
+    
+    {
+        title: 'Projects',
+        icon: folder,
+        window: '..'
+
+    },
+    
+    {
+        title: 'Pictures',
+        icon: pics,
+        window: '..'
+
+    },
+
+    {
+        title: 'Soemthing',
+        icon: notes,
+        window: '..'
+
+    }
+];
+
