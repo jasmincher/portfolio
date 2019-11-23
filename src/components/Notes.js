84a33cabb49a7dfa3a7e8e3dbabf773c772d4a18
Jas <c.jasminher@gmail.com>
2019-11-22 22:24:31 -0500

    Created a Notes component that contains a text area

---
+++ src/components/Notes.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../sass/Notes.scss';
+
+
+class Notes extends React.Component {
+    render() {
+        return (
+            <div className="notes-container"> 
+              
+                    <textarea>
+                        hello this is a text area
+                    </textarea>
+
+
+                </div>
+
+
+        )
+    }
+
+}
+
+export default Notes;
