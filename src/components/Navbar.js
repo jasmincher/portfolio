110846b3f6a876ac7f809ad75675549256572167
Jas <c.jasminher@gmail.com>
2019-11-22 22:38:26 -0500

    Added a clock component from grommet, and styled to be on the right

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-
-
+import '../sass/Navbar.scss';
+import { Clock } from 'grommet';
 
 class Navbar extends React.Component{
     render(){
+        
+       
         return (
-            <div>
-                <NavLink exact to="/"> Home </NavLink>
-                <NavLink to="/projects"> Projects </NavLink>
-
+            <div className="nav-container">
+                <NavLink exact to="/" className="inactive" activeClassName="active"> <i className="material-icons" style={{fontSize: '1.3rem'}}>home</i> </NavLink>
+                <NavLink to="/settings" className="inactive" activeClassName="active"> Settings</NavLink>
+                <Clock alignSelf="center" hourLimit={12} size="small" type="digital" style={{ marginLeft: 'auto', color: 'black', fontWeight:'600'}}/>
             </div>
 
         )
