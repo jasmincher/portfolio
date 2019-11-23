6b61f0a0fbd1e90c62d26e059c0f40cf6281c47f
Jas <c.jasminher@gmail.com>
2019-11-22 22:23:36 -0500

    Created Icon component to contain an image, title and a function to hide or show a window

---
+++ src/components/Icon.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../sass/Icon.scss';
+
+
+function Icon(props) {
+    return (
+        <div className="icon-container" onClick={props.onIconClick}>
+            <img src={props.icon} className="icon-img" />
+            <p className="icon-title">{props.title}</p>
+        </div>
+    )
+}
+
+export default Icon;
