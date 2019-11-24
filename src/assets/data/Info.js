d580ba381f0917379b28bdf5116aecd99a0a9d6d
Jas <c.jasminher@gmail.com>
2019-11-24 00:33:47 -0500

    Added info for projects, such as project name and links, for test only, will modify later..

---
+++ src/assets/data/Info.js
@@ -0,0 +1,38 @@
+export const Links = 
+[
+    {
+        projectName: "Project 1",
+        href: "https://www.w3schools.com/cssref/css_selectors.asp"
+
+    },
+
+    {
+        projectName: "Project 2",
+        href: "https://www.w3schools.com/cssref/css_selectors.asp"
+
+    },
+
+    {
+        projectName: "Project 3",
+        href: "https://www.w3schools.com/cssref/css_selectors.asp"
+
+    },
+
+    {
+        projectName: "Project 4",
+        href: "https://www.w3schools.com/cssref/css_selectors.asp"
+
+    },
+
+    {
+        projectName: "Project 5",
+        href: "https://www.w3schools.com/cssref/css_selectors.asp"
+
+    },
+
+    {
+        projectName: "Project 6",
+        href: "https://www.w3schools.com/cssref/css_selectors.asp"
+
+    },
+]
