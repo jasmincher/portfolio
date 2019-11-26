e9cd8f98ca3cd011e3da311e1470258088d760b6
Jas <c.jasminher@gmail.com>
2019-11-26 14:11:16 -0500

    Removed some white space

--- src/components/Section.js
+++ src/components/Section.js
@@ -2,17 +2,15 @@ import React from 'react';
 import Icon from './Icon';
 import '../sass/Section.scss'
 
-
-
 function Section(props) {
 
-    const items = props.icons.map(i => <Icon window={i.window} title={i.title} icon={i.icon}  onIconClick={props.onItemClick.bind(this, i)} />)
+    const items = props.icons.map(i => <Icon window={i.window} title={i.title} icon={i.icon} onIconClick={props.onItemClick.bind(this, i)} />)
 
     return (
         <div className="icons">
-           {items}
+            {items}
         </div>
     );
 }
 
-export default Section;
+export default Section; 
