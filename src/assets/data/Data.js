5ec3fa6dc1874ae353aa71e59cffe5e890c30d98
Jas <c.jasminher@gmail.com>
2019-12-02 23:38:45 -0500

    Nothing changed

--- src/assets/data/Data.js
+++ src/assets/data/Data.js
@@ -2,6 +2,7 @@ import notes from '../images/Text 2.png';
 import folder from '../images/Folder.png';
 import email from '../images/Message.png';
 import pics from '../images/Pics.png';
+import settings from '../images/Settings Values.png'
 
 
 export const Icons = 
@@ -34,9 +35,9 @@ export const Icons =
     },
 
     {
-        title: 'Soemthing',
-        icon: notes,
-        component: 'Notes',
+        title: 'Settings',
+        icon: settings,
+        component: 'Settings',
     }
 ];
 
--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import '../sass/Icon.scss';
 
+
 function Icon(props) {
   
     return (
+       
         <div className="icon-container" onClick={props.onIconClick}>
             <img src={props.icon} className="icon-img" />
             <p className="icon-title">{props.title}</p>
         </div>
+       
     )
 }
 
