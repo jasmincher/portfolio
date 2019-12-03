6f95b56d595e30e41d59b438ad47d1ddc24edfda
Jas <c.jasminher@gmail.com>
2019-12-03 14:29:17 -0500

    Created mail component

---
+++ src/components/Mail.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Mail extends React.Component {
+    render(){
+        return(
+            <div>
+                I am mail
+            </div>
+        )
+    }
+}
+
+export default Mail;
