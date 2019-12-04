1597ba2aaac8148b036e98dba5550ccfdb6e495c
Jas <c.jasminher@gmail.com>
2019-12-03 22:06:25 -0500

    Removed state and functions to handle change and handle submit

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -4,21 +4,10 @@ import '../sass/Notes.scss';
 
 class Notes extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = { value: 'This is a text area' };
-
-        this.handleChange = this.handleChange.bind(this);
-    }
-
-    handleChange(e) {
-        this.setState({ value: e.target.value });
-    }
-
     render() {
         return (
             <div className="notes-container">
-                <textarea value={this.state.value} onChange={this.handleChange}></textarea>
+                <textarea> This is a text area</textarea>
             </div>
         )
     }
