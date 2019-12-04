93297cc0ac2661ba9052ccad518461f088c01af2
Jas <c.jasminher@gmail.com>
2019-12-03 21:30:10 -0500

    Added state to keep track of user input for text area, fixing the issue of not being able to edit the textarea

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -3,18 +3,23 @@ import '../sass/Notes.scss';
 
 
 class Notes extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { value: 'This is a text area' };
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({ value: e.target.value });
+    }
+
     render() {
         return (
-            <div className="notes-container"> 
-              
-                    <textarea>
-                        hello this is a text area
-                    </textarea>
-
-
-                </div>
-
-
+            <div className="notes-container">
+                <textarea value={this.state.value} onChange={this.handleChange}></textarea>
+            </div>
         )
     }
 
