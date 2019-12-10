158243fc145ed6686047b298c1d44c1f8f7e249e
Jas <c.jasminher@gmail.com>
2019-12-10 18:58:35 -0500

    Added comments

--- src/components/Window.js
+++ src/components/Window.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import '../sass/Window.scss';
-import Draggable from 'react-draggable';
-
+import React from "react";
+import "../sass/Window.scss";
+import Draggable from "react-draggable";
 
 class Window extends React.Component {
-
-    render() {
-
-        return (
-
-            <Draggable handle={this.props.handle}>
-                <div className="window-container">
-                    {this.props.children}
-                </div>
-            </Draggable>
-        )
-    }
+  render() {
+    return (
+      //handles where to drag component
+      <Draggable handle={this.props.handle}>
+        <div className="window-container">{this.props.children}</div>
+      </Draggable>
+    );
+  }
 }
-
-
-
 
 export default Window;
