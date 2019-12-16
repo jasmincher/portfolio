070b0c7b4fce5695de92cc9db4651d76fe8bb2a5
Jas <c.jasminher@gmail.com>
2019-12-16 00:42:35 -0500

    Edited notes component

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import '../sass/Notes.scss';
-
+import React from "react";
+import "../sass/Notes.scss";
 
 class Notes extends React.Component {
-
-    render() {
-        return (
-            <div className="notes-container">
-                <textarea> This is a text area</textarea>
-            </div>
-        )
-    }
-
+  render() {
+    return (
+      <div className="notes-container">
+        <textarea>
+          Write down any notes here...
+   
+        </textarea>
+      </div>
+    );
+  }
 }
 
 export default Notes;
