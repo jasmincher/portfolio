bd915a6e53b11dedfe6969ab8076c8430568d393
Jas <c.jasminher@gmail.com>
2019-12-16 15:19:08 -0500

    Updates

---
+++ precache-manifest.cce274f9b08d07ec396bb5479d24f841.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b90dfbaf56acaa1a4a958d87869704e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "76340f8004bceb50f193",
+    "url": "/portfolio/static/css/2.97089d65.chunk.css"
+  },
+  {
+    "revision": "8898f3cf74dd76b4a982",
+    "url": "/portfolio/static/css/main.fb2f3f1c.chunk.css"
+  },
+  {
+    "revision": "76340f8004bceb50f193",
+    "url": "/portfolio/static/js/2.b2c0841d.chunk.js"
+  },
+  {
+    "revision": "8898f3cf74dd76b4a982",
+    "url": "/portfolio/static/js/main.b913ce06.chunk.js"
+  },
+  {
+    "revision": "6043226c40dcb0adf1d9",
+    "url": "/portfolio/static/js/runtime-main.4fc83baf.js"
+  },
+  {
+    "revision": "afdc7112f89f2b2951af9dfa96b67f2d",
+    "url": "/portfolio/static/media/About.afdc7112.jpeg"
+  },
+  {
+    "revision": "2e725282f771f6eafa4181b628f6585b",
+    "url": "/portfolio/static/media/Jasmin Hernandez - Resume.2e725282.pdf"
+  },
+  {
+    "revision": "e005dae1c8754edfd4976b7cf5b04391",
+    "url": "/portfolio/static/media/Resume.e005dae1.png"
+  }
+]);
