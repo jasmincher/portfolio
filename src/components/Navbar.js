3a6380138663204a7c666e54f7aa5302605ed879
Jas <c.jasminher@gmail.com>
2019-12-15 21:51:53 -0500

    Made navbar height smaller, and removed navlink component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,22 @@
-import React from "react";
-import "../sass/Navbar.scss";
-import { Clock } from "grommet";
-import { NavLink } from "reactstrap";
+import React from 'react';
+import '../sass/Navbar.scss';
+import { Clock } from 'grommet';
 
 class Navbar extends React.Component {
   
   render() {
     return (
       <div className="nav-container">
-        <NavLink exact to="/">
-          <i className="material-icons" style={{ fontSize: "1.3rem" }}>
+          <i className="material-icons" style={{ fontSize: '1.3rem', alignSelf: 'center'}}>
             home
           </i>
-        </NavLink>
 
         <Clock
           alignSelf="center"
           hourLimit={12}
           size="small"
           type="digital"
-          style={{ marginLeft: "auto", color: "black", fontWeight: "400" }}
+          style={{ marginLeft: 'auto', color: 'black', fontWeight: '400' }}
         />
       </div>
     );
