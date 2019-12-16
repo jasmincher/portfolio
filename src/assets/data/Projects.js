c0ee963571f36d493ca6420ec7b4cda21f937b0d
Jas <c.jasminher@gmail.com>
2019-12-15 21:04:33 -0500

    Updated projects info

---
+++ src/assets/data/Projects.js
@@ -0,0 +1,34 @@
+export const Links = 
+[
+    {
+        projectName: "Must Watch TV Shows and Movies",
+        href: "https://topmoviesandshows.herokuapp.com/"
+
+    },
+
+    {
+        projectName: "IGLOO Events",
+        href: "https://polar-inlet-74763.herokuapp.com/"
+
+    },
+
+    {
+        projectName: "Check It Games",
+        href: "https://checkit-games.herokuapp.com/"
+
+    },
+
+    {
+        projectName: "SeeMe",
+        href: "https://seeme-tch.herokuapp.com/"
+
+    },
+
+    {
+        projectName: "Meetup Connect",
+        href: "https://jasmincher.github.io/meetup-connect/"
+
+    },
+
+
+]
