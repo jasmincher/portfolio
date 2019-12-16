137152f9b81f786bbe876e5f2d4c39e7bf176120
Jas <c.jasminher@gmail.com>
2019-12-15 22:33:04 -0500

    Changed import name for project links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../sass/Projects.scss'
 import { Chrome } from 'grommet-icons';
-import { Links } from '../assets/data/Info';
+import { Links } from '../assets/data/Projects';
 
 class Projects extends React.Component {
 
