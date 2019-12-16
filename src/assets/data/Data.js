29e2e8bceb3ea67ebbed4d055eecf0bb2d5ef906
Jas <c.jasminher@gmail.com>
2019-12-15 21:11:55 -0500

    Updated data.js with about component

--- src/assets/data/Data.js
+++ src/assets/data/Data.js
@@ -3,7 +3,7 @@ import folder from '../images/Folder.png';
 import email from '../images/Message.png';
 import pics from '../images/Pics.png';
 import settings from '../images/Settings Values.png'
-
+import resume from '../images/Text 7.png';
 
 export const Icons = 
 [
@@ -29,15 +29,21 @@ export const Icons =
     },
     
     {
-        title: 'Pictures',
+        title: 'About',
         icon: pics,
-        component: 'Notes',
+        component: 'About',
     },
 
     {
         title: 'Settings',
         icon: settings,
         component: 'Settings',
+    },
+
+    {
+        title: 'Resume',
+        icon: resume,
+        component: 'Resume',
     }
 ];
 
