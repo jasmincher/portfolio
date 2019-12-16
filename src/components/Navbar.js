bdef2d4ad4842555f125acbcded7c9f3795c145b
Jas <c.jasminher@gmail.com>
2019-12-15 20:56:10 -0500

    Removed functions to change font weight on click, and removed the about link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,71 +1,29 @@
-import React from 'react';
-import '../sass/Navbar.scss';
-import { Clock } from 'grommet';
-import { NavLink } from 'reactstrap'
-import Notes from './Notes'
-import Window from './Window'
+import React from "react";
+import "../sass/Navbar.scss";
+import { Clock } from "grommet";
+import { NavLink } from "reactstrap";
 
 class Navbar extends React.Component {
+  
+  render() {
+    return (
+      <div className="nav-container">
+        <NavLink exact to="/">
+          <i className="material-icons" style={{ fontSize: "1.3rem" }}>
+            home
+          </i>
+        </NavLink>
 
-    constructor(props) {
-        super(props)
-        this.state = {
-           clicked: null,
-           active: false
-        };
-    }
-
-
-    isClicked = (id) => {
-        if (this.state.clicked === id) {
-            this.setState({ clicked: null });
-            
-        } else {
-            this.setState({ clicked: id, active: true })
-
-        }
-
-    }
-
-
-
-    renderW = () => {
-        if (this.state.active == true) {
-            return <Notes/>
-        }
-    }
-
-
-    changeFont = (id) => {
-        if (this.state.clicked === id) {
-            return "500"
-        } return " ";
-    }
-
-
-
-
-
-    render() {
-
-
-        return (
-            <div className="nav-container">
-
-                {/* {this.renderW()} */}
-                <NavLink exact to="/" style={{fontWeight: this.changeFont(0)}} onClick={() => {this.isClicked(0)}}> <i className="material-icons" style={{ fontSize: '1.3rem' }}>home</i> </NavLink>
-                <NavLink style={{fontWeight: this.changeFont(1)}} onClick={() => {this.isClicked(1)}}>About</NavLink>
-
-
-
-
-
-                <Clock alignSelf="center" hourLimit={12} size="small" type="digital" style={{ marginLeft: 'auto', color: 'black', fontWeight: '600' }} />
-            </div>
-
-        )
-    }
-
+        <Clock
+          alignSelf="center"
+          hourLimit={12}
+          size="small"
+          type="digital"
+          style={{ marginLeft: "auto", color: "black", fontWeight: "400" }}
+        />
+      </div>
+    );
+  }
 }
 
 export default Navbar;
