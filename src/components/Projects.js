b90c9eca232f38a5ca38de51a9842a2d64ffe62a
Jas <c.jasminher@gmail.com>
2019-12-15 23:27:37 -0500

    Styled projects component and added div around heading for name

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,30 +1,33 @@
-import React from 'react';
-import '../sass/Projects.scss'
-import { Chrome } from 'grommet-icons';
-import { Links } from '../assets/data/Projects';
+import React from "react";
+import "../sass/Projects.scss";
+import { Chrome } from "grommet-icons";
+import { Links } from "../assets/data/Projects";
 
 class Projects extends React.Component {
 
+
+  
   render() {
+    const projects = Links.map(i => (
+      <li>
+        <Chrome color="plain" size="small" style={{marginRight: '5px'}} />
+        <a href={i.href} target="_blank">
+          {i.projectName}
+        </a>
+      </li>
+    ))
 
     return (
-
       <div className="projects-container">
-
-        <p>Name</p>
-
+        <div className="file-details">
+          <h6 style={{ fontSize: "12px", marginBottom: "5px" }}>Name</h6>
+        </div>
         <ul>
-
-          {Links.map(i => (
-            <li> <Chrome color="plain" size="small"/> <a href={i.href} target="_blank">{i.projectName}</a></li>
-          ))}
-
+          {projects}
         </ul>
-
       </div>
-    )
+    );
   }
 }
-
 
 export default Projects;
