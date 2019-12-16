54e370729ad593ead882e5e80d381e6d1e8d6751
Jas <c.jasminher@gmail.com>
2019-12-15 22:35:01 -0500

    Deleted unused pictures

---
+++ src/assets/data/Colors.js
@@ -0,0 +1,19 @@
+import variables from "../../sass/Settings.scss";
+
+export const gradients = [
+  { color: variables.defaultcolor },
+  { color: variables.gradient1 },
+  { color: variables.gradient2 },
+  { color: variables.gradient3 },
+  { color: variables.gradient4 }
+];
+
+
+
+export const colors = [
+    {color: variables.pink},
+    {color: variables.blue},
+    {color: variables.purple},
+    {color: variables.green},
+    {color: `url('https://bit.ly/2EfRnf9')`}
+];
