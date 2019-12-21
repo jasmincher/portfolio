8b69a0c87d26ed4a55d74e675838f56ca1388e96
Jas <c.jasminher@gmail.com>
2019-12-20 23:13:03 -0500

    Commented out Navbar component

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Home from './pages/Home';
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      {/* <Navbar /> */}
 
       <Switch>
         <Route exact path="/portfolio" component={Home} />
