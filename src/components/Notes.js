b01580c2607b57433f2ce32fb6e7a480e8417780
Jas <c.jasminher@gmail.com>
2019-12-20 22:47:37 -0500

    Changed text area to div with editable content, and styled this div, as it was with the text area before.

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -4,11 +4,19 @@ import "../sass/Notes.scss";
 class Notes extends React.Component {
   render() {
     return (
-      <div className="notes-container">
-        <textarea>
-          Write down any notes here...
-   
-        </textarea>
+      <div
+        className="notes-container"
+        contentEditable="true"
+        spellcheck="false"
+      >
+        <p>Write down any notes here...</p>
+
+        <p>Did you know? </p>
+        <p>
+          That the AMBER Alert is a backronym for America's missing: Broadcast
+          Emergency Response, named after Amber Hagerman, a 9 year old girl who
+          was abducted and murdered in Arlingyon, Texas, in 1996.
+        </p>
       </div>
     );
   }
