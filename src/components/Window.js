9303dd169be7f8844e508ccbc9964220fb6127b6
Jas <c.jasminher@gmail.com>
2020-03-08 22:22:26 -0400

    feat/style: Added bounds to draggable component so that it doesnot go over or under navbar display

--- src/components/Window.js
+++ src/components/Window.js
@@ -4,9 +4,10 @@ import Draggable from "react-draggable";
 
 class Window extends React.Component {
   render() {
+
     return (
       //handles where to drag component
-      <Draggable handle={this.props.handle}>
+      <Draggable handle={this.props.handle} bounds="parent">
         <div className="window-container">{this.props.children}</div>
       </Draggable>
     );
