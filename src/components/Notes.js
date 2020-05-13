855f505bb45bf0767f507163c1614b9665adfa0f
Jas <c.jasminher@gmail.com>
2020-05-12 20:43:20 -0400

    refactor: Added localStorage to let user change text of notes or reset if it is blank

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -2,19 +2,29 @@ import React from "react";
 import "../sass/Notes.scss";
 
 class Notes extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: `Did you know? 
+
+That the AMBER Alert is a backronym for America's missing: Broadcast Emergency Response, named after Amber Hagerman, a 9 year old girl who was abducted and murdered in Arlington, Texas, in 1996`,
+    };
+  }
+
+  componentDidMount() {
+    const notes = localStorage.getItem("notes") || this.state.notes;
+    document.getElementById("notepad").value = notes;
+  }
+
+  changeNotes() {
+    const notes = document.getElementById("notepad").value;
+    localStorage.setItem("notes", notes);
+  }
+
   render() {
     return (
-      <div
-        className="notes-container"
-        contentEditable="true"
-        spellcheck="false"
-      >
-        <p>Did you know? </p>
-        <p>
-          That the AMBER Alert is a backronym for America's missing: Broadcast
-          Emergency Response, named after Amber Hagerman, a 9 year old girl who
-          was abducted and murdered in Arlington, Texas, in 1996.
-        </p>
+      <div className="notes-container" spellCheck="false">
+        <textarea id="notepad" onChange={() => this.changeNotes()}></textarea>
       </div>
     );
   }
