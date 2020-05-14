136b39005ab7d01d58d27e122da2e0b738479622
Jas <c.jasminher@gmail.com>
2020-05-14 00:43:15 -0400

    Updates

---
+++ precache-manifest.9ccec5b824d81c61910487365ae3da63.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6690a52fb91575b784b40d0714ee9752",
+    "url": "/c605d34eaf9e16b750a0.worker.js"
+  },
+  {
+    "revision": "f8c808a5db41b1c175ac90ac8a6aeeba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eed2ab3ab8ec3d9f014a",
+    "url": "/static/css/2.fe1dd3bb.chunk.css"
+  },
+  {
+    "revision": "d36a0b8dcfe8ae8e1409",
+    "url": "/static/css/main.62a04ea8.chunk.css"
+  },
+  {
+    "revision": "eed2ab3ab8ec3d9f014a",
+    "url": "/static/js/2.b73241c9.chunk.js"
+  },
+  {
+    "revision": "d36a0b8dcfe8ae8e1409",
+    "url": "/static/js/main.d16829a3.chunk.js"
+  },
+  {
+    "revision": "a0fb235a6af033195f09",
+    "url": "/static/js/runtime-main.33611d63.js"
+  },
+  {
+    "revision": "afdc7112f89f2b2951af9dfa96b67f2d",
+    "url": "/static/media/About.afdc7112.jpeg"
+  },
+  {
+    "revision": "2e725282f771f6eafa4181b628f6585b",
+    "url": "/static/media/Jasmin Hernandez - Resume.2e725282.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.da1d54f1eddecf0a7934d6426bf368b8.js"
+  "/precache-manifest.9ccec5b824d81c61910487365ae3da63.js"
 );
 
 self.addEventListener('message', (event) => {
