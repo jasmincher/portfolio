e32004ddee9ce87d7c3247d7c34a03460bae56e8
Jas <c.jasminher@gmail.com>
2020-05-14 20:35:39 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.06551dd690add5f1a91931ff5ce761c3.js"
+  "/precache-manifest.acebad604f324a8127221b9c929da413.js"
 );
 
 self.addEventListener('message', (event) => {
