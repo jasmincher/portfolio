1c2878a6e9f46703bf98d511f32e32f2cbaccc9b
Jas <c.jasminher@gmail.com>
2020-09-14 17:25:17 -0400

    Updated About info

--- src/components/About.js
+++ src/components/About.js
@@ -12,15 +12,15 @@ class About extends React.Component {
         <img src={about}/>
 
         <p>
-          I'm currently a senior studying computer science. My passions lie in
-          frontend, web design, and security, however I'm open to learning and
+          I'm a recent graduate with a BS in Computer Science. My passions lie in
+          frontend, web design, and security. However, I'm open to learning and
           exploring other branches in technology. One of my career goals is to
           use my programming skills to one day help the life of at least one
           person.
         </p>
 
         <p>
-          In my freetime, I like to do my nails, play with my lovely dogs, 5
+          In my freetime, I like to do my nails, rollerskate, play with my lovely dogs, 5
           dogs to be exact (yes 5, I love animals), and I also like to watch
           true crime. Watching true crime is so fascinating, yet, also very sad,
           especially when it comes to cases on missing people. These cases on
