9fab20cc3fae3c7dda4a1c0c1f3a8009ce15701d
Jas <c.jasminher@gmail.com>
2020-09-14 17:24:33 -0400

    Updated projects information

--- src/assets/data/Projects.js
+++ src/assets/data/Projects.js
@@ -2,32 +2,31 @@ export const Links =
 [
     {
         projectName: "The Best Movies and Shows",
-        href: "https://watch-it.now.sh/"
+        href: "https://watch-it.now.sh/",
+    },
 
+    {
+        projectName: "Open Source Portfolio Template",
+        href: "https://github.com/jasmincher/portfolio-template",
     },
 
     {
         projectName: "IGLOO Events",
-        href: "https://polar-inlet-74763.herokuapp.com/"
-
-    },
+        href: "https://polar-inlet-74763.herokuapp.com/",    },
 
     {
         projectName: "Check It Games",
-        href: "https://checkit-games.herokuapp.com/"
-
+        href: "https://checkit-games.herokuapp.com/",
     },
 
     {
         projectName: "SeeMe",
-        href: "https://see-me.now.sh/"
-
+        href: "https://see-me.now.sh/",
     },
 
     {
         projectName: "Meetup Connect",
-        href: "https://meetup-connect.now.sh/"
-
+        href: "https://meetup-connect.now.sh/",
     },
 
 
